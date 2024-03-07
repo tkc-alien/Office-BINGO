6fbee57e28417fecb06ec7d236b868ecd25d157d
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-03-08 02:08:45 +0900

    add: ホーム画面の枠を作成

---
+++ lib/feature/home/home_builder.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:office_bingo/feature/home/home_screen.dart';
+
+class HomeBuilder {
+  static Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
